1859e33d8f00bd11d475b1d9ca8ef11863806ad8
Tanju BOZOK <tnjbzk@gmail.com>
2022-11-29 18:59:13 +0300

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TodoAddComponent } from './pages/todo-add/todo-add.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodoAddComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
